027cdabe1bed93278ac4d11f9232cb7c6e661e2f
Percy.Ku <percyku19@gmail.com>
2023-05-23 22:43:32 +0800

    add 3.selectionsort.js

--- 2.Sorting Algorithms part1/3.selectionsort.js
+++ 2.Sorting Algorithms part1/3.selectionsort.js
@@ -2,4 +2,19 @@ let unsorted = [14, -4, 17, 6, 22, 1, -5];
 
 selectionSort(unsorted);
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    let temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
+    console.log(arr);
+  }
+  console.log(arr);
+  return arr;
+}
