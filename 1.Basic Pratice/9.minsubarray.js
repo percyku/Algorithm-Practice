5a0403d3642f4073e95dfaf7c65bbdb3b81709f4
Percy.Ku <percyku19@gmail.com>
2023-05-27 21:42:51 +0800

    add 9.minsubarray.js

---
+++ 1.Basic Pratice/9.minsubarray.js
@@ -0,0 +1,32 @@
+function minSubArray(arr, sum) {
+  let start = 0;
+  let end = 0;
+  let total = 0;
+  let minLength = Infinity;
+
+  while (start < arr.length) {
+    if (total < sum && end < arr.length) {
+      total += arr[end];
+      end++;
+    } else if (total >= sum) {
+      let currentLength = end - start;
+      if (minLength > currentLength) {
+        minLength = currentLength;
+      }
+      total -= arr[start];
+      start++;
+    } else if (end >= arr.length) {
+      break;
+    }
+  }
+
+  if (minLength === Infinity) {
+    console.log("You can't find subarray in this array:" + arr);
+    return 0;
+  } else {
+    console.log(minLength);
+    return minLength;
+  }
+}
+
+minSubArray([8, 1, 6, 15, 3, 16, 5, 7, 14, 30, 12], 70);
